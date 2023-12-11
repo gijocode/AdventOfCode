43cf18cc817851f7e3dce67dd26662a6e2cfd41f
gijocode <gijocode@gmail.com>
2023-12-11 23:21:33 +0530

    q8

--- q8/main.py
+++ q8/main.py
@@ -4,23 +4,14 @@ for m in map_list.splitlines():
     k, v = m.split(" = ")
     v = v.strip("()")
     mapper[k] = list(v.split(", "))
+
 total_steps, index = 0, 0
 val = "AAA"
-"""
-while val != "ZZZ":
-    total_steps += 1
-    val = mapper[val][0 if instructions[0] == "L" else 1]
-    instructions = instructions[1:] + instructions[0]
-print(total_steps)
-"""
 l = len(instructions)
 instructions = list(map(lambda x: 1 if x == "R" else 0, instructions))
-print(instructions)
-print(mapper)
+
 while not val == "ZZZ":
     val = mapper[val][instructions[index]]
     index = (index + 1) % l
-    print(val, index)
     total_steps += 1
-    # print("Finding Z for ", val, current)
 print(total_steps)
